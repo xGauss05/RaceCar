d9acc3cc6d05e599950c07cb9c2961cfcc8d7806
xGauss05 <jcacaynes@gmail.com>
2023-01-29 13:41:13 +0100

    Changed jump value.

--- ModulePlayer.cpp
+++ ModulePlayer.cpp
@@ -231,11 +231,8 @@ update_status ModulePlayer::Update(float dt)
 
 	if (App->input->GetKey(SDL_SCANCODE_SPACE) == KEY_DOWN)
 	{
-		if (activeImpulse) {
-			float jump = 10000000 * dt * dt;
-			LOG("dt = %f; jump = %f", dt, jump);
-			vehicle->Push(0, jump, 0);
-		}
+		if (activeImpulse) vehicle->Push(0, 3000, 0);
+		
 		if (!playJumpFx)
 		{
 			playJumpFx = true;
